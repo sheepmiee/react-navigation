dfbbea41f0341d78e02d9b16e0847051a66668a9
sheepmiee <954939058@qq.com>
2017-09-12 15:11:05 +0800

    Update TabBarBottom.js
    change for my project

--- src/views/TabView/TabBarBottom.js
+++ src/views/TabView/TabBarBottom.js
@@ -1,7 +1,7 @@
 /* @flow */
 
-import React, { PureComponent } from 'react';
-import { Animated, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import React, {PureComponent} from 'react';
+import {Animated, TouchableWithoutFeedback, StyleSheet, View, TouchableOpacity, Image, Text} from 'react-native';
 import TabBarIcon from './TabBarIcon';
 
 import type {
@@ -13,7 +13,7 @@ import type {
   TextStyleProp,
 } from '../../TypeDefinition';
 
-import type { TabScene } from './TabView';
+import type {TabScene} from './TabView';
 
 type DefaultProps = {
   activeTintColor: string,
@@ -40,11 +40,9 @@ type Props = {
   showIcon: boolean,
 };
 
-export default class TabBarBottom extends PureComponent<
-  DefaultProps,
+export default class TabBarBottom extends PureComponent<DefaultProps,
   Props,
-  void
-> {
+  void> {
   // See https://developer.apple.com/library/content/documentation/UserExperience/Conceptual/UIKitUICatalog/UITabBar.html
   static defaultProps = {
     activeTintColor: '#3478f6', // Default active tint color in iOS 10
@@ -69,8 +67,8 @@ export default class TabBarBottom extends PureComponent<
     if (showLabel === false) {
       return null;
     }
-    const { index } = scene;
-    const { routes } = navigation.state;
+    const {index} = scene;
+    const {routes} = navigation.state;
     // Prepend '-1', so there are always at least 2 items in inputRange
     const inputRange = [-1, ...routes.map((x: *, i: number) => i)];
     const outputRange = inputRange.map(
@@ -83,17 +81,17 @@ export default class TabBarBottom extends PureComponent<
     });
 
     const tintColor = scene.focused ? activeTintColor : inactiveTintColor;
-    const label = this.props.getLabel({ ...scene, tintColor });
+    const label = this.props.getLabel({...scene, tintColor});
     if (typeof label === 'string') {
       return (
-        <Animated.Text style={[styles.label, { color }, labelStyle]}>
+        <Animated.Text style={[styles.label, {color}, labelStyle]}>
           {label}
         </Animated.Text>
       );
     }
 
     if (typeof label === 'function') {
-      return label({ ...scene, tintColor });
+      return label({...scene, tintColor});
     }
 
     return label;
@@ -134,44 +132,81 @@ export default class TabBarBottom extends PureComponent<
       style,
       tabStyle,
     } = this.props;
-    const { routes } = navigation.state;
+    const {routes} = navigation.state;
     // Prepend '-1', so there are always at least 2 items in inputRange
     const inputRange = [-1, ...routes.map((x: *, i: number) => i)];
     return (
-      <Animated.View style={[styles.tabBar, style]}>
-        {routes.map((route: NavigationRoute, index: number) => {
-          const focused = index === navigation.state.index;
-          const scene = { route, index, focused };
-          const outputRange = inputRange.map(
-            (inputIndex: number) =>
-              inputIndex === index
-                ? activeBackgroundColor
-                : inactiveBackgroundColor
-          );
-          const backgroundColor = position.interpolate({
-            inputRange,
-            outputRange: (outputRange: Array<string>),
-          });
-          const justifyContent = this.props.showIcon ? 'flex-end' : 'center';
-          return (
-            <TouchableWithoutFeedback
-              key={route.key}
-              onPress={() => jumpToIndex(index)}
-            >
-              <Animated.View
-                style={[
-                  styles.tab,
-                  { backgroundColor, justifyContent },
-                  tabStyle,
-                ]}
+      <View>
+        <Animated.View style={[styles.tabBar, style]}>
+          {routes.map((route: NavigationRoute, index: number) => {
+            const focused = index === navigation.state.index;
+            const scene = {route, index, focused};
+            const outputRange = inputRange.map(
+              (inputIndex: number) =>
+                inputIndex === index
+                  ? activeBackgroundColor
+                  : inactiveBackgroundColor
+            );
+            const backgroundColor = position.interpolate({
+              inputRange,
+              outputRange: (outputRange: Array<string>),
+            });
+            const justifyContent = this.props.showIcon ? 'flex-end' : 'center';
+            return (
+              <TouchableWithoutFeedback
+                key={route.key}
+                onPress={() => jumpToIndex(index)}
               >
-                {this._renderIcon(scene)}
-                {this._renderLabel(scene)}
-              </Animated.View>
-            </TouchableWithoutFeedback>
-          );
-        })}
-      </Animated.View>
+                <Animated.View
+                  style={[
+                    styles.tab,
+                    {
+                      backgroundColor,
+                      justifyContent,
+                      marginLeft: index == 2 ? 25 : 0,
+                      marginRight: index == 1 ? 25 : 0
+                    },
+                    tabStyle,
+                  ]}
+                >
+                  {this._renderIcon(scene)}
+                  {this._renderLabel(scene)}
+                </Animated.View>
+              </TouchableWithoutFeedback>
+            );
+          })}
+        </Animated.View>
+
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={this.props.buttonPress}
+          style={{
+            position: 'absolute',
+            top: -20,
+            left: gSize.width / 2 - 30,
+            backgroundColor: '#fff',
+            height: 60,
+            width: 60,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 30}}>
+          <View style={{
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: '#1296db',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <Image source={require('../../../../../src/resource/icons/add.png')}
+                   style={{width: 30, height: 30, borderRadius: 15,}}/>
+          </View>
+
+        </TouchableOpacity>
+
+
+      </View>
+
     );
   }
 }
@@ -180,7 +215,7 @@ const styles = StyleSheet.create({
   tabBar: {
     height: 49, // Default tab bar height in iOS 10
     flexDirection: 'row',
-    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopWidth: 0,
     borderTopColor: 'rgba(0, 0, 0, .3)',
     backgroundColor: '#F7F7F7', // Default background color in iOS 10
   },
